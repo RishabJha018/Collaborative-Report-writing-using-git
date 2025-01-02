ba3ffd1cad1e54c1cbe4d764ca5c9e42220fee49
RishabJha018 <1nt23is179.rishab@nmit.ac.in>
2025-01-03 01:07:00 +0530

    changed

--- testdemo.js
+++ testdemo.js
@@ -1,2 +1,3 @@
-Benefits of Git in Collaborative Development.
-Historically, collaborating on code involved manual file sharing or maintaining multiple versions. If you’re lucky, you don’t know how confusing and inefficient this often turned out to be. Git addressed these challenges by providing central code storage and records of changes.
+console.log("Benefits of Git in Collaborative Development.
+Historically, collaborating on code involved manual file sharing or maintaining multiple versions.\n If you’re lucky, you don’t know how
+confusing and inefficient this often turned out to be.\n Git addressed these challenges by providing central code storage and records of changes.")
