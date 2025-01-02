aed2690f348eb8d6b37b4637c886286608a70882
RishabJha018 <1nt23is179.rishab@nmit.ac.in>
2025-01-03 01:02:54 +0530

    commited

---
+++ testdemo.js
@@ -0,0 +1,2 @@
+Benefits of Git in Collaborative Development.
+Historically, collaborating on code involved manual file sharing or maintaining multiple versions. If you’re lucky, you don’t know how confusing and inefficient this often turned out to be. Git addressed these challenges by providing central code storage and records of changes.
